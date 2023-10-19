f3ef5de402d5e59f9b38cb2ba2c180af0f6d77cf
Molly Pate <63977049+mpate13@users.noreply.github.com>
2023-10-18 20:39:18 -0400

    updated resources used

--- calendar/month/MonthView.swift
+++ calendar/month/MonthView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+/*
+    Used the following resources:
+    https://developer.apple.com/documentation/swiftui/environmentvalues/calendar
+    https://sarunw.com/posts/swiftui-tabview/
+    https://codingwithrashid.com/how-to-align-vstack-items-to-top-in-ios-swiftui/
+    https://www.hackingwithswift.com/forums/swiftui/creating-a-daily-view-calendar-with-overlapping-events/22436
+    https://blog.logrocket.com/working-calendars-swift/#create-swift-ui-project
+    https://www.swiftyplace.com/blog/tabview-in-swiftui-styling-navigation-and-more
+*/
+
 struct MonthView: View {
     @State var selectedDate: Date = Date()
     var body: some View {
