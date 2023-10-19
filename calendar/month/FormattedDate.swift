4aa3b16e0577a0baf34beba3f94fad44d93fd5b3
Molly Pate <63977049+mpate13@users.noreply.github.com>
2023-10-18 20:39:45 -0400

    updated resources used

--- calendar/month/FormattedDate.swift
+++ calendar/month/FormattedDate.swift
@@ -1,5 +1,15 @@
 import SwiftUI
 
+/*
+    Used the following resources:
+    https://developer.apple.com/documentation/swiftui/environmentvalues/calendar
+    https://sarunw.com/posts/swiftui-tabview/
+    https://codingwithrashid.com/how-to-align-vstack-items-to-top-in-ios-swiftui/
+    https://www.hackingwithswift.com/forums/swiftui/creating-a-daily-view-calendar-with-overlapping-events/22436
+    https://blog.logrocket.com/working-calendars-swift/#create-swift-ui-project
+    https://www.swiftyplace.com/blog/tabview-in-swiftui-styling-navigation-and-more
+*/
+
 struct FormattedDate: View {
     var selectedDate: Date
     var omitTime: Bool = false
