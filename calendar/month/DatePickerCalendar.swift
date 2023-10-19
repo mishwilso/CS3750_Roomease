17066f0dd209ade2b60006c3dc38b9a48379231e
Molly Pate <63977049+mpate13@users.noreply.github.com>
2023-10-18 20:38:58 -0400

    updated resources used

--- calendar/month/DatePickerCalendar.swift
+++ calendar/month/DatePickerCalendar.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+/*
+    Used the following resources:
+    https://developer.apple.com/documentation/swiftui/environmentvalues/calendar
+*/
+
 struct DatePickerCalendar: View {
     @State var selectedDate = Date()
     var body: some View {
