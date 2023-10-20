8b8602141a8a64c6e0801d0b9c4cd29a7649ff00
Mish Wilson <112350999+mishwilso@users.noreply.github.com>
2023-10-20 09:59:51 -0700

    Files!! Uploaded by Wish

---
+++ Roomease/Roomease/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by user247737 on 10/16/23.
+//
+
+import SwiftUI
+
+
+let lightGray = Color(red: 0.8, green: 0.8, blue: 0.8)
+
+let lighterGray = Color(red: 0.9, green: 0.9, blue: 0.9)
+
+let bttnColor = Color(red: 0.8, green: 0.8, blue: 0.8)
+
+let textColor = Color(red: 0, green: 0, blue: 0)
+
+let white = Color(red: 1, green: 1, blue: 1)
+
+
+struct ContentView: View {
+    var body: some View {
+        SignInUpView()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ Roomease/Roomease/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by user247737 on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavStackView()
+        }
+    }
+}
