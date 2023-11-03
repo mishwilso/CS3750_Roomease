687cc50445a8e3b505ab12eaa9ef8d0389911ff3
dkelley3 <112639728+dkelley3@users.noreply.github.com>
2023-11-03 14:00:15 -0400

    I added more firebase functions to AuthService

--- Roomease/View Model/AuthService.swift
+++ Roomease/View Model/AuthService.swift
@@ -39,7 +39,7 @@ class AuthService {
                 let user = authResult.user
                 
                 //TODO: Make Firestore class
-                let userData: [String: Any] = ["HouseID": 10, "firstName": firstname, "lastName": lastname]
+                let userData: [String: Any] = ["HouseID": 10, "firstName": firstname, "lastName": lastname, "Chores": [], "Groceries": [], "Events": [], "Messages": []]
                 
                 let db = Firestore.firestore()
                 
@@ -54,6 +54,286 @@ class AuthService {
             }
         
     }
+    
+    func addChore(userID: String, task: String, frequency: String, deadline: String) async throws {
+        
+        do {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userID)
+            
+            let documentSnapshot = try await userDocument.getDocument()
+            
+            if var currentData = documentSnapshot.data() as? [String: Any] {
+                
+                var chores: [[String: Any]] = currentData["Chores"] as? [[String: Any]] ?? []
+                
+                let newChore: [String: Any] = ["Task": task, "Frequency": frequency, "Deadline": deadline]
+                
+                chores.append(newChore)
+                
+                currentData["Chores"] = chores
+                
+                try await userDocument.setData(currentData)
+                
+                print("Chore added")
+                
+            }
+            else {
+                print("Chore array not found.")
+                
+            }
+        }
+        catch {
+            print("Error adding chore: \(error.localizedDescription)")
+            throw DBError.choreAddFailed(errorMessage: error.localizedDescription)
+        }
+        
+    }
+    
+    func addEvent(userID: String, day: Int, endTime: String, month: Int, startTime: Int, title: String, year: Int) async throws {
+        do {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userID)
+            
+            let documentSnapshot = try await userDocument.getDocument()
+            
+            if var currentData = documentSnapshot.data as? [String: Any] {
+                var events: [[String: Any]] = currentData["Events"] as? [[String: Any]] ?? []
+                
+                let newEvent: [String: Any] = ["Day": day, "EndTime": endTime, "Month": month, "StartTime": startTime, "Title": title, "Year": year]
+                
+                events.append(newEvent)
+                
+                currentData["Events"] = events
+                
+                try await userDocument.setData(currentData)
+                
+                print("Event added")
+            }
+            else {
+                print("Event array not found.")
+            }
+        }
+        catch {
+            print("Error adding event: \(error.localizedDescription)")
+            throw DBErrir.eventAddFailed(errorMessage: error.localizedDescription)
+        }
+        
+    }
+    
+    func addGrocery(userID: string, item: String, purpose: String, store: String) async throws {
+        do {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userID)
+            
+            let documentSnapshot = try await userDocument.getDocument()
+            
+            if var currentData = documentSnapshot.data as? [String: Any] {
+                var groceries: [[String: Any]] = currentData["Groceries"] as? [[String: Any]] ?? []
+                
+                let newGrocery: [String: Any] = ["Item": item, "Purpose": purpose, "Store": store]
+                
+                groceries.append(newGrocery)
+                
+                currentData["Groceries"] = groceries
+                
+                try await userDocument.setData(currentData)
+                
+                print("Grocery added")
+            }
+            else {
+                print("Grocery array not found.")
+            }
+        }
+        catch {
+            print("Error adding grocery: \(error.localizedDescription)")
+            throw DBError.groceryAddFailed(errorMessage: error.localizedDescription)
+        }
+    }
+    
+    func addMessage(userID: String, messageID: String, message: String, groupID: String, received: Bool, timeStamp: Date, isPinned: Bool) async throws {
+        do {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userID)
+            
+            let documentSnapshot = try await userDocument.getDocument()
+            
+            if var currentData = documentSnapshot.data as? [String: Any] {
+                var messages: [[String: Any]] = currentData["Messages"] as? [[String: Any]] ?? []
+                
+                let newMessage: [String: Any] = ["Message": message, "GroupID": groupID, "Received": received, "TimeStamp": timeStamp, "IsPinned": isPinned]
+                
+                messages.append(newMessage)
+                
+                currentData["Messages"] = messages
+                try await userDocument.setData(currentData)
+                
+                print("Message added.")
+                
+            }
+            else {
+                print("Message array not found.")
+            }
+        }
+        catch {
+            print("Error adding message: \(error.localizedDescription)")
+            throw DBError.messageAddFailed(errorMessage: error.localizedDescription)
+        }
+    }
+    
+    func getChores(userUID: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userUID)
+
+            userDocument.getDocument { (documentSnapshot, error) in
+                if let error = error {
+                    print("Error getting document: \(error.localizedDescription)")
+                    completion(nil, error)
+                    return
+                }
+
+                if let documentData = documentSnapshot?.data(),
+                   let chores = documentData["Chores"] as? [[String: Any]] {
+                    completion(chores, nil)
+                } else {
+                    // Chores array not found or user document doesn't exist
+                    completion(nil, nil)
+                }
+            }
+        }
+    
+    func getEvents(userID: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        let userDocument = db.collection("Users").document(userID)
+        
+        userDocument.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                print("Error getting document: \(error.localizedDescription)")
+                completion(nil, error)
+                return
+            }
+            
+            if let documentData = documentSnapshot?.data(),
+               let events = documentData["Events"] as? [[String: Any]] {
+                completion(events, nil)
+            } else {
+                
+                completion(nil, nil)
+            }
+        }
+    }
+    
+    func getGroceries(userID: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        let userDocument = db.collection("Users").document(userID)
+        
+        userDocument.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                print("Error getting document: \(error.localizedDescription)")
+                completion(nil, error)
+                return
+            }
+            
+            if let documentData = documentSnapshot?.data(),
+               let groceries = documentData["Groceries"] as? [[String: Any]] {
+                completion(groceries, nil)
+            } else {
+                
+                completion(nil, nil)
+            }
+        }
+    }
+    
+    func getMessages(userID: String, completion: @escaping ([[String: Any]]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        let userDocument = db.collection("Users").document(userID)
+        
+        userDocument.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                print("Error getting document: \(error.localizedDescription)")
+                completion(nil, error)
+                return
+            }
+            
+            if let documentData = documentSnapshot?.data(),
+               let messages = documentData["Messages"] as? [[String: Any]] {
+                completion(messages, nil)
+            } else {
+                
+                completion(nil, nil)
+            }
+        }
+    }
+    
+    func deleteChore(userUID: String, task: String, completion: @escaping (Error?) -> Void) {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userUID)
+
+            userDocument.updateData([
+                "Chores": FieldValue.arrayRemove([["Task": task]])
+            ]) { error in
+                if let error = error {
+                    print("Error deleting chore: \(error.localizedDescription)")
+                    completion(error)
+                } else {
+                    print("Chore deleted successfully")
+                    completion(nil)
+                }
+            }
+        }
+    
+   
+    func deleteGrocery(userUID: String, item: String, completion: @escaping (Error?) -> Void) {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userUID)
+
+            userDocument.updateData([
+                "Groceries": FieldValue.arrayRemove([["Item": task]])
+            ]) { error in
+                if let error = error {
+                    print("Error deleting grocery: \(error.localizedDescription)")
+                    completion(error)
+                } else {
+                    print("Grocery deleted successfully")
+                    completion(nil)
+                }
+            }
+        }
+    
+    func deleteEvent(userUID: String, title: String, completion: @escaping (Error?) -> Void) {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userUID)
+
+            userDocument.updateData([
+                "Events": FieldValue.arrayRemove([["Title": task]])
+            ]) { error in
+                if let error = error {
+                    print("Error deleting event: \(error.localizedDescription)")
+                    completion(error)
+                } else {
+                    print("Event deleted successfully")
+                    completion(nil)
+                }
+            }
+        }
+    
+    func deleteMessage(userUID: String, timeStamp: Date, completion: @escaping (Error?) -> Void) {
+            let db = Firestore.firestore()
+            let userDocument = db.collection("Users").document(userUID)
+
+            userDocument.updateData([
+                "Messages": FieldValue.arrayRemove([["TimeStamp": task]])
+            ]) { error in
+                if let error = error {
+                    print("Error deleting event: \(error.localizedDescription)")
+                    completion(error)
+                } else {
+                    print("Event deleted successfully")
+                    completion(nil)
+                }
+            }
+        }
+    
 }
         
 
