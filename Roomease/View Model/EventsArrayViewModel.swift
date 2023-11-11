a8bc84cd74ad17345bf017faf64a0a419ec35449
dkelley3 <112639728+dkelley3@users.noreply.github.com>
2023-11-11 15:55:56 -0500

    Created EventsArrayViewModel

---
+++ Roomease/View Model/EventsArrayViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  EventsArrayViewModel.swift
+//  
+//
+//  Created by Daniel Kelley on 11/11/23.
+//
+
+import Foundation
+
+class EventsArrayViewModel: ObservableObject {
+    
+    @Published var events: [[String: Any]] = []
+    
+    func getTheEvents() {
+        print("Attempting to get events.")
+        
+        AuthService.shared.getEvents {[weak self] (events, error) in
+            guard let self = self else { return }
+            
+            if let error = {
+                print("Error getting the events: \(error)")
+            }
+            
+            else if let events = events {
+                self.events = events
+            }
+            
+            else {
+                print("No events found.")
+            }
+        }
+    }
+}
