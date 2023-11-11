5935852023a35ce30310fb3aad1111cd4ab8d561
Mish Wilson <112350999+mishwilso@users.noreply.github.com>
2023-11-11 17:57:45 -0500

    Update ChatViewModel.swift - from Mish

--- Roomease/View Model/ChatViewModel.swift
+++ Roomease/View Model/ChatViewModel.swift
@@ -15,46 +15,19 @@ class ChatViewModel: ObservableObject {
     let db = Firestore.firestore()
     
     init() {
-        getChatMessages()
-    }
-    
-    func getChatMessages() {
-        db.collection("ChatMessages").addSnapshotListener { documentSnapshot, error in
-            guard let documents = documentSnapshot?.documents else {
-                print("Error fetching chat messages.")
-                return
-            }
-            
-            self.chatMessages = documents.compactMap { document -> MessageModel? in
-                do {
-                    return try document.data(as: MessageModel.self)
-                } catch {
-                    print("Could not decode document into ChatMessage.")
-                    return nil
-                }
-            }
-            
-            self.chatMessages.sort {
-                $0.timestamp > $1.timestamp
-            }
-            
-            if let mostRecentMessageID = self.chatMessages.first?.id {
-                self.mostRecentChatMessageID = mostRecentMessageID
-            }
+        Task.init {
+            await self.getChatMessages()
         }
     }
     
-    func sendMessage(group_id: String, sender: String, text: String, received: Bool, pinned: Bool) {
-        if text == "" {
-            return
+    func getChatMessages() async {
+        await AuthService.shared.getChatMessages {updatedMessages, updatedMostRecentID in
+            self.chatMessages = updatedMessages
+            self.mostRecentChatMessageID = updatedMostRecentID
         }
-        
-        do {
-            let newMessage = MessageModel(id: "\(UUID())", group_id: group_id, sender: sender, text: text, received: received, timestamp: Date(), pinned: pinned)
-            
-            try db.collection("ChatMessages").document().setData(from: newMessage)
-        } catch {
-            print("Error adding chat message to Firestore database.")
-        }
+    }
+    
+    func sendMessage(group_id: String, sender: String, text: String, received: Bool, pinned: Bool) async {
+        await AuthService.shared.sendMessage(group_id: group_id, sender: sender, text: text, received: received, pinned: pinned)
     }
 }
